29a7af4e9b0f187a67db55200efa04f14b2716ac
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-09-15 13:06:36 +0900

    feat: fix design & add post-area-component

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>
+  <div class="container">
     <div class="side-area">
       <p>side-area</p>
       <Sidebar />
     </div>
     <div class="post-area">
       <p>post-area</p>
+      <Content />
+      <Form />
     </div>
   </div>
 </template>
 
 <script>
 import Sidebar from '~/components/Sidebar.vue'
+import Form from '~/components/Form.vue'
+
 export default {
-  components: { Sidebar }
+  components: { Sidebar, Form }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: flex-start;
+}
+
+.side-area {
+  width: 300px;
+  position: sticky;
+  top: 0;
+}
+
+.post-area {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  border-left: solid 1px rgba(255, 255, 255, 0.1);
+  padding: 16px 16px 0;
+}
+</style>
