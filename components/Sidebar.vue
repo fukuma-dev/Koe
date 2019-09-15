8a67b41e670449c04062398c8d2c2bcf8b2c95c3
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-09-15 12:19:33 +0900

    feat: add Sidebar-Component

---
+++ components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sidebar">
+    <div v-if="user" class="sidebar-user">
+      <p>user_image</p>
+      <p>user_name</p>
+    </div>
+    <v-btn v-if="user === false" @click="handleSignin">Sign in</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: false // 本当はnullにする
+    }
+  },
+  methods: {
+    hadleSignin() {}
+  }
+}
+</script>
+
+<style scoped>
+.sidebar {
+  padding: 16px;
+}
+
+.sidebar-user {
+  display: flex;
+  font-size: 14px;
+  font-weight: bold;
+  align-items: center;
+  justify-content: flex-start;
+}
+</style>
