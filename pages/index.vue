021ed279e9a1ae828ef1a01b1bcb61715d229aab
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-09-15 12:19:10 +0900

    feat: create main-page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="side-area">
+      <p>side-area</p>
+      <Sidebar />
+    </div>
+    <div class="post-area">
+      <p>post-area</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/Sidebar.vue'
+export default {
+  components: { Sidebar }
+}
+</script>
